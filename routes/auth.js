320a30e4eaffa3cfb206714e08fa01b9be2ae640
Achmad Fahreza <81659348+achreza@users.noreply.github.com>
2023-03-12 13:10:02 +0700

    register berhasil

--- routes/auth.js
+++ routes/auth.js
@@ -13,11 +13,89 @@ app.get("/", function (req, res, next) {
 });
 app.get("/success", (req, res) => res.render("pages/success", { layout: "layouts/layout", title: "GreenTech", page: "success", user: userProfile }));
 app.get("/error", (req, res) => res.send("error logging in"));
+app.get("/errorSQL", (req, res) => res.send("error in SQL Query"));
 app.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 
 app.get("/google/callback", passport.authenticate("google", { failureRedirect: "/auth/error" }), function (req, res) {
   // Successful authentication, redirect success.
   res.redirect("/auth/success");
+});
+
+app.post("/register", function (req, res) {
+  // req.assert("email", "email is required").notEmpty(); //Validate email
+  // req.assert("fullname", "fullname is required").notEmpty(); //Validate fullname
+  // req.assert("phone_number", "phone_number is required").notEmpty(); //Validate phone_number
+  // req.assert("institution", "institution is required").notEmpty(); //Validate institution
+
+  // var errors = req.validationErrors();
+
+  // if (!errors) {
+  //No errors were found.  Passed Validation!
+
+  /********************************************
+		 * Express-validator module
+		 
+		req.body.comment = 'a <span>comment</span>';
+		req.body.bobotid_kriteria = '   a bobot    ';
+
+		req.sanitize('comment').escape(); // returns 'a &lt;span&gt;comment&lt;/span&gt;'
+		req.sanitize('bobotid_kriteria').trim(); // returns 'a bobot'
+		********************************************/
+  var user = {
+    email: req.sanitize("email").escape().trim(),
+    nama: req.sanitize("fullname").escape().trim(),
+    no_telp: req.sanitize("phone_number").escape().trim(),
+    institusi: req.sanitize("institution").escape().trim(),
+    negara: req.sanitize("country").escape().trim(),
+    jenis_kelamin: req.sanitize("gender").escape().trim(),
+    id_role_user: req.sanitize("category").escape().trim(),
+  };
+  console.log(user);
+
+  req.getConnection(function (error, conn) {
+    conn.query(
+      "INSERT INTO `icgt`.`m_user` (`email`, `nama`, `no_telp`, `institusi`, `negara`, `jenis_kelamin`, `id_role_user`) VALUES ('" +
+        user.email +
+        "', '" +
+        user.nama +
+        "', '" +
+        user.no_telp +
+        "', '" +
+        user.institusi +
+        "', '" +
+        user.negara +
+        "', '" +
+        user.jenis_kelamin +
+        "', " +
+        user.id_role_user +
+        ");",
+      function (err, result) {
+        //if(err) throw err
+        if (err) {
+          req.flash("error", err);
+
+          res.redirect("/auth/errorSQL");
+        } else {
+          req.flash("success", "Data added successfully!");
+          //redirect ke dashboard user
+          res.render("auth/", { layout: "layouts/layout", title: "GreenTech", page: "auth" });
+        }
+      }
+    );
+  });
+  // } else {
+  //   var error_msg = "";
+  //   errors.forEach(function (error) {
+  //     error_msg += error.msg + "<br>";
+  //   });
+  //   req.flash("error", error_msg);
+
+  //   /**
+  //    * Using req.body.id_kriteria
+  //    * because req.param('id_kriteria') is deprecated
+  //    */
+  //   res.redirect("/auth/error");
+  // }
 });
 
 passport.serializeUser(function (user, cb) {
